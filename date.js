a05073500f808de2fdd90440840430146977e75d
Antra Gupta <90148290+Antra-Gupta@users.noreply.github.com>
2023-03-21 12:33:29 +0530

    Add files via upload
    Initial Commit

---
+++ date.js
@@ -0,0 +1,17 @@
+
+exports.getDay =function(){
+        const today=new Date();
+           const options={
+                weekday:"long",
+            };
+            return today.toLocaleDateString("en-US",options);
+        };
+exports.getDate=function(){
+            let today=new Date();
+                let options={
+                    weekday:"long",
+                    day:"numeric",
+                    month:"long"
+                };
+                return today.toLocaleDateString("en-US",options);
+            };
